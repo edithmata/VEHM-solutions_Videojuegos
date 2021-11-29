835b183840efe6d93da506aacf5f12385f4ffecd
Christian01224 <14960@virtual.utsc.edu.mx>
2021-11-28 21:51:37 -0600

    Valeria Garcia VIdeojuegos

---
+++ Valeria Garia-14960/Assets/Scripts/teleport.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject Player;
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        Player.transform.position = teleportTarget.transform.position;
+    }
+}
